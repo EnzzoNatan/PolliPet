facff5f74d8206cfce8b902b90be48cf8aec8bc0
EnzzoNatan <enzzoferrazz1909@gmail.com>
2025-11-02 09:36:42 -0300

    Alteração das rotas e tornando,  SPA responsiva e troca das imagens

--- spa/script.js
+++ spa/script.js
@@ -1,35 +1,37 @@
-    let telaAnterior = 'tela-home'
-    let telaAtual = 'tela-home'
+let telaAnterior = 'tela-home'
+let telaAtual = 'tela-home'
 
-   function navegar(destino){
-       let telas = document.getElementsByClassName('tela')
-       Array.from(telas).forEach(element => {
-           element.classList.remove('show')
-           element.classList.add('collapse')
-       });
-       document.getElementById(destino).classList.remove('collapse')
-       document.getElementById(destino).classList.add('show')
-       telaAnterior = telaAtual
-       telaAtual = destino
-    }
+function navegar(destino) {
+  document.querySelectorAll('.tela').forEach(t => {
+    t.classList.remove('show')
+    t.classList.add('collapse')
+  })
 
-    function voltar() {
-        navegar(telaAnterior)
-    }
+  document.getElementById(destino).classList.remove('collapse')
+  document.getElementById(destino).classList.add('show')
 
-     function mostrarDetalhes(produto, imagem, categoria, preco){
-        navegar('tela-produto')
-        let detalhes = document.getElementById('detalhes-produto')
-        detalhes.innerHTML =`
-            <div class="row g-3">
-                <div class="col-md-4 text-center">
-                <img src="${imagem}" class="img-fluid" alt="${produto}">
-                </div>
-                <div class="col-md-8">
-                <h2>${produto}</h2>
-                <p><strong>Categoria:</strong> ${categoria}</p>
-                <p><strong>Preço:</strong> R$ ${preco}</p>
-                </div>
-            </div>
-        `
-    }
+  telaAnterior = telaAtual
+  telaAtual = destino
+}
+
+function voltar() {
+  navegar(telaAnterior)
+}
+
+function mostrarDetalhes(produto, imagem, categoria, preco) {
+  navegar('tela-produto')
+
+  let detalhes = document.getElementById('detalhes-produto')
+  detalhes.innerHTML = `
+    <div class="row g-3">
+      <div class="col-md-4 text-center">
+        <img src="${imagem}" class="img-fluid rounded" alt="${produto}">
+      </div>
+      <div class="col-md-8">
+        <h2>${produto}</h2>
+        <p><strong>Categoria:</strong> ${categoria}</p>
+        <p><strong>Preço:</strong> ${preco}</p>
+      </div>
+    </div>
+  `
+}
